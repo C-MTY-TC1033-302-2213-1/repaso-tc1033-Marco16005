a7f942cb75b34b61bb6b5a5ad4436c5041bc2440
Marco16005 <a00839451@tec.mx>
2024-05-14 01:15:50 -0600

    Revert "Solución Marco Antonio Torres"
    This reverts commit c24a70194a663e97e1cb51c5143cb6351063c7a7.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 //
 //  main.cpp
 //  ComposicionTriangulo
+//
+//  Created by Ma. Guadalupe Roque Díaz de León on 18/05/23.
+//
 
 #include <iostream>
 #include <iomanip> // for std::setprecision()
-#include "punto.hpp"
-#include "triangulo.hpp"
+#include "Punto.hpp"
+#include "Triangulo.hpp"
 
 using namespace std;
-
 int main( ){
     Punto p1{-5, -5}, p2{1, 3}, p3{4, -6};
     Punto p4{-2, -3}, p5{5, -4}, p6{-1,3};
@@ -16,7 +18,6 @@ int main( ){
     Triangulo tri1{p4,p5,p6}, tri2{p1, p2, p3}, tri3{p7, p8, p9};
     
     cout << "p_tri1 = " << tri1.perimetro() << endl;
-
     cout << "a_tri1 = " << tri1.area() << endl;
     cout << "p_tri2 = " << tri2.perimetro() << endl;
     cout << "a_tri2 = " << tri2.area() << endl;
@@ -26,3 +27,4 @@ int main( ){
    
     return 0;
 }
+
